70ba514e1288b8efb40d3d2fafa23b81cab75505
AttilaHun63 <gal.attika0201@gmail.com>
2020-04-23 12:44:06 +0200

    04

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $(document).on('submit', function(){
-        if($("#headercinput").val() == "random"){
+        if($("#headercinput").val().toLowerCase() == "random"){
             var r = Math.floor(Math.random() * 255) + 1;
             var g = Math.floor(Math.random() * 255) + 1;
             var b = Math.floor(Math.random() * 255) + 1;
             $("#header").css("background", "rgb(" + r + ", " + g + ", " + b + ")");
-        }else if($("#headercinput").val() == "def"){
+        }else if($("#headercinput").val().toLowerCase() == "def"){
             $("#header").css("background", "rgb(100, 0, 255)");
         }else{
 
